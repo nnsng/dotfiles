53fb12b101bf827f032d967acf4edebd19801d71
nnsng <ngocsang1201@gmail.com>
2024-07-28 19:47:49 +0700

    wezterm: add config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.font = wezterm.font("MesloLGS NF")
+config.font_size = 14
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 1
+config.macos_window_background_blur = 10
+
+return config
