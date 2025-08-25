ecec9812d3860978f28d44cf7e7db496bca4eb8c
nnsng <ngocsang1201@gmail.com>
2025-08-25 22:42:31 +0700

    nvim: update config

--- nvim.lazy/.config/nvim/lua/config/autocmds.lua
+++ nvim.lazy/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable autoformat for any project inside .../dxc/helix/
+vim.api.nvim_create_autocmd("BufReadPre", {
+  callback = function()
+    local cwd = vim.fn.getcwd()
+    if cwd:match("/dxc/helix/") then
+      vim.g.autoformat = false
+    end
+  end,
+})
