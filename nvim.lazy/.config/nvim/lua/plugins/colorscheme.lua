c020ebd196f25397d1d8ed55ed3db77af4d40233
nnsng <ngocsang1201@gmail.com>
2025-09-06 00:11:49 +0700

    nvim: transparent background

--- nvim.lazy/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim.lazy/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,15 @@
 return {
   {
     "catppuccin/nvim",
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+        float = {
+          transparent = true,
+          solid = true,
+        },
+      })
+    end,
     opts = function(_, opts)
       local module = require("catppuccin.groups.integrations.bufferline")
       if module then
